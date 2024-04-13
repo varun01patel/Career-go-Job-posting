5ab1936a3960583897f02c5ab8fca262fd6f675e
varun01patel <varun01patel@gmail.com>
2024-04-13 11:47:55 +0530

    navbar updated

--- src/Component/Navbar/index.jsx
+++ src/Component/Navbar/index.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <AppBar position="static" color="primary">
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Career Go.
+          Go Careers.
         </Typography>
         <Hidden smDown> {/* Hide for mobile */}
           {/* Additional navigation links or icons */}
